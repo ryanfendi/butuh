4e204da3a6f1219ccc5d7a127fc91977e47b9890
ryanfendi <102475046+ryanfendi@users.noreply.github.com>
2026-08-25 21:36:21 +0700

    Refactor Firebase initialization code and imports
    Refactor import statements and format code for readability. Added a console log to confirm Firebase initialization.

--- firebase.js
+++ firebase.js
@@ -1,53 +1,63 @@
 // ============================================================
 // FIREBASE.JS
-// Marketplace Kebutuhan
+// BUTUH - Marketplace Kebutuhan
 // ============================================================
 
-import { initializeApp } from
-  "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { getAuth } from
-  "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { getFirestore } from
-  "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // ============================================================
 // FIREBASE CONFIG
 // ============================================================
-// GANTI dengan konfigurasi Web App Firebase Anda.
-// Firebase Console
-// → Project Settings
-// → Your apps
-// → Web app
-// ============================================================
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyCUFHkwOfNo-JCGHWG3dWcnISoYLg7vGnY",
+
   authDomain: "butuhin.firebaseapp.com",
+
   projectId: "butuhin",
+
   storageBucket: "butuhin.firebasestorage.app",
+
   messagingSenderId: "331896660506",
+
   appId: "1:331896660506:web:7a03f433101b81dd74e7a3"
+
 };
 
 // ============================================================
-// INITIALIZE FIREBASE
+// INITIALIZE
 // ============================================================
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(
+  firebaseConfig
+);
 
 // ============================================================
 // AUTH
 // ============================================================
 
-const auth = getAuth(app);
+const auth = getAuth(
+  app
+);
 
 // ============================================================
 // FIRESTORE
 // ============================================================
 
-const db = getFirestore(app);
+const db = getFirestore(
+  app
+);
 
 // ============================================================
 // EXPORT
@@ -58,3 +68,7 @@ export {
   auth,
   db
 };
+
+console.log(
+  "✅ Firebase berhasil diinisialisasi"
+);
